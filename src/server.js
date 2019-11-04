40a63e6475c3c3bab7f7cf63fc45bfb552400715
MatiasDevop <solomatias321@gmail.com>
2019-11-04 18:26:17 +0000

    update heroku

--- src/server.js
+++ src/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
  
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/angularapp'));
+app.use(express.static(__dirname + '/dist/RestaurantNG8'));
  
 app.get('/*', function(req,res) {
  
-res.sendFile(path.join(__dirname+'/angularapp/index.html'));
+res.sendFile(path.join(__dirname+'/dist/RestaurantNG8/index.html'));
 });
  
 // Start the app by listening on the default Heroku port
